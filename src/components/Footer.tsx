9bd09697aecdbcba44b3e50535f4511544d74a16
JPimia <90615229+JPimia@users.noreply.github.com>
2025-03-04 11:46:37 +0200

    added social links with icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Social_Icons } from "../constant/socials";
 
 const FooterWrapper = styled.footer`
     width: 100%;
@@ -20,11 +21,33 @@ const Highlight = styled.span`
     color: transparent;
 `;
 
+const SocialIconsWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-bottom: 10px;
+`;
+
+const SocialIconLink = styled.a`
+    margin: 0 10px;
+    z-index: 0;
+`;
+
+const SocialIconImage = styled.img`
+    height: 30px;
+    width: 30px;
+`;
+
 const Footer: React.FC = () => (
     <FooterWrapper>
+        <SocialIconsWrapper>
+            {Social_Icons.map((social) => (
+                <SocialIconLink href={social.link} key={social.alt}>
+                    <SocialIconImage src={social.image} alt={social.alt} />
+                </SocialIconLink>
+            ))}
+        </SocialIconsWrapper>
         <Paragraph>
-            Made with <Highlight>React</Highlight> and{" "}
-            <Highlight>Styled Components</Highlight>
+            Made with ❤️ using <Highlight>React</Highlight>{" "}
         </Paragraph>
     </FooterWrapper>
 );
