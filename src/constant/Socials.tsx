52513cccbed672681f119940489c76708f6001f1
JPimia <90615229+JPimia@users.noreply.github.com>
2025-03-04 11:47:08 +0200

    added socials as constants

---
+++ src/constant/Socials.tsx
@@ -0,0 +1,15 @@
+import github from "../assets/images/github-mark-white.png";
+import linkedin from "../assets/images/linkedin.png";
+
+export const Social_Icons = [
+    {
+        link: "https://www.linkedin.com/in/jari-pimia/",
+        image: linkedin,
+        alt: "LinkedIn",
+    },
+    {
+        link: "https://github.com/JPimia",
+        image: github,
+        alt: "GitHub",
+    },
+];
