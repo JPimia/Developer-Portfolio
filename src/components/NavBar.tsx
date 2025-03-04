04158ae6cc118e5816401474b1b6f86d70b2aac7
JPimia <90615229+JPimia@users.noreply.github.com>
2025-03-04 11:47:20 +0200

    navbar tunings

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import signature from "../assets/images/signature.png";
 
 const NavBarWrapper = styled.header`
     display: flex;
@@ -17,23 +18,44 @@ const NavBarWrapper = styled.header`
 const Logo = styled.div`
     font-size: 1.5rem;
     font-weight: bold;
+    margin-left: -25px;
+    margin-top: 10px;
+    margin-bottom: -25px;
 `;
 
-const Nav = styled.nav`
+const Signature = styled.img`
+    display: flex;
+    width: 250px;
+    align-self: center;
+    justify-self: center;
+`;
+
+const ContactButton = styled.nav`
     a {
-        border: 1px solid #fff;
+        display: flex;
         font-size: 1rem;
-        padding: 5px 15px;
-        border-radius: 20px;
+        font-weight: 500;
+        font-family: "Poppins", sans-serif;
+        letter-spacing: 0.15px;
+        color: rgb(212, 212, 212);
+        padding: 10px 30px;
+        border-radius: 10px;
+        transform: scaleX(1.2);
+        &:hover {
+            background-color: rgba(112, 112, 112, 0.4);
+        }
+        margin-right: 25px;
     }
 `;
 
 const Header: React.FC = () => (
     <NavBarWrapper>
-        <Logo>Jari Pimiä</Logo>
-        <Nav>
-            <a href="#contact">Contact</a>
-        </Nav>
+        <Logo>
+            <Signature src={signature} alt="signature" />
+        </Logo>
+        <ContactButton>
+            <a href="mailto:jari.pimiaa@gmail.com">Contact</a>
+        </ContactButton>
     </NavBarWrapper>
 );
 
